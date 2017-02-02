1d72e00fb6bde2694e52f3a34ce84da42f598ab8
sonjaB28 <burkart.sonja@gmail.com>
2017-02-02 11:26:38 +0100

    iah 10

--- js/script.js
+++ js/script.js
@@ -13,9 +13,11 @@ function changeImage(alpha, beta, gamma) {
 	
 	var x_pos = canvasWidth/2 - half_width;
 	var y_pos = canvasHeight/2 - half_height;	
+	new_x_pos = updatePosition(x_pos,gamme);
+	new_y_pos = updatePosition(y_pos,beta);
 	
 	context.clearRect(0, 0, canvasWidth, canvasHeight);
-	context.drawImage(img, updatePosition(x_pos,gamme), updatePosition(y_pos,beta));
+	context.drawImage(img, new_x_pos, new_y_pos);
 }
 
 function updatePosition(position, value) {
