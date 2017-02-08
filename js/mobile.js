f1df55c5b8099de63be5712cc07b4bd9706f4ee7
sonjaB28 <burkart.sonja@gmail.com>
2017-02-08 17:50:01 +0100

    reconstruct js 6

--- js/mobile.js
+++ js/mobile.js
@@ -2,9 +2,7 @@
 
 
 
-window.addEventListener("deviceorientation", deviceOrientationChanged(event), true);
-
-function deviceOrientationChanged(event) {
+window.addEventListener("deviceorientation", function(event) {
 	var alpha = event.alpha;
 	var beta = event.beta;
 	var gamma = event.gamma;
@@ -13,4 +11,5 @@ function deviceOrientationChanged(event) {
 	document.querySelector("#mag_alpha").innerHTML = "alpha = " + alpha;
 	document.querySelector("#mag_beta").innerHTML = "beta = " + beta;
 	document.querySelector("#mag_gamma").innerHTML = "gamma = " + gamma;
-}
+}, true);
+
