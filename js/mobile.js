b06eaa93ab5218c75d4e87928420fbc93368e15e
sonjaB28 <burkart.sonja@gmail.com>
2017-02-08 18:01:44 +0100

    reconstruct js 9

--- js/mobile.js
+++ js/mobile.js
@@ -51,10 +51,22 @@ window.addEventListener("deviceorientation", function(event) {
 	var alpha = event.alpha;
 	var beta = event.beta;
 	var gamma = event.gamma;
+	
+	if(doCalibrate == true) {
+		alpha_standard = alpha;
+		beta_standard = beta;
+		gamma_standard = gamma;
+		doCalibrate = false;
+	}
 
+	drawActor();
 
 	document.querySelector("#mag_alpha").innerHTML = "alpha = " + alpha;
 	document.querySelector("#mag_beta").innerHTML = "beta = " + beta;
 	document.querySelector("#mag_gamma").innerHTML = "gamma = " + gamma;
 }, true);
 
+
+function drawActor() {
+	context.drawImage(actorImg, 0, 0);
+}
