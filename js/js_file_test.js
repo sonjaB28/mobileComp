995b2541d54911eb24dc54629196c2d97155fe5e
sonjaB28 <burkart.sonja@gmail.com>
2017-02-08 13:52:56 +0100

    repair magnetometer16

--- js/js_file_test.js
+++ js/js_file_test.js
@@ -1,16 +1,9 @@
 
-x_pos = 5;
-y_pos = 5;
 alpha_standard = 0;
 beta_standard = 0;
 gamma_standard = 0;
 
 
-
-movement = 6;
-range = 2;
-doCalibrate = true;
-text = "start: ";
 // Mobile Usage 
 
 window.addEventListener("deviceorientation", function(event) {
@@ -19,51 +12,14 @@ window.addEventListener("deviceorientation", function(event) {
 	var gamma = event.gamma;
 	
 
-	var actorImg = new Image();
-	actorImg.src = "img/iah.jpg";
+	var actorImg2 = new Image();
+	actorImg2.src = "img/iah.jpg";
 	var c2 = document.getElementById("canvas");
-	context = c2.getContext("2d");
+	context2 = c2.getContext("2d");
+	context2.translate(15,0);
+	context2.drawImage(actorImg2, 0, 0);
 	
-	drawActor();
-	
-	//update(alpha-alpha_standard, beta-beta_standard, gamma-gamma_standard);
-
 	document.querySelector("#mag_alpha").innerHTML = "alpha = " + alpha;
 	document.querySelector("#mag_beta").innerHTML = "beta = " + beta;
 	document.querySelector("#mag_gamma").innerHTML = "gamma = " + gamma;
-	
-}
-
-// other functions
-
-function update(alpha, beta, gamma) {
-	
-	updatePosition(x_pos, y_pos, gamma, beta);
-	
-	context.setTransform(1,0,0,1, x_pos, y_pos);
-		
-	drawActor();
-}
-
-function drawActor() {
-	context.drawImage(actorImg, 0, 0);
-}
-
-function updatePosition(x, y, gamma, beta) {
-	var x_dir = getDirection(gamma);
-	var y_dir = getDirection(beta);	
-	
-	x_pos += x_dir * 4;
-	y_pos += y_dir*4;
-}
-
-function getDirection(orientation) {
-	if(orientation < -range) {
-		return -1;
-	} else if(orientation > range) {
-		return  1;
-	} else {
-		return 0;	
-	}
-}
-
+}, true);
