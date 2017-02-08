d59c5fc7a6779e4cf74ffbda89697921ba4fae0c
sonjaB28 <burkart.sonja@gmail.com>
2017-02-08 13:39:55 +0100

    repair magnetometer10

--- js/js_file_test.js
+++ js/js_file_test.js
@@ -6,25 +6,8 @@ gamma_standard = 0;
 
 // Mobile Usage 
 
-window.addEventListener("deviceorientation", deviceOrientationChanged(event), true);
-
-function deviceOrientationChanged(event) {
-
-	
-	var alpha = event.alpha;
-	var beta = event.beta;
-	var gamma = event.gamma;
-	
-
-	var actorImg2 = new Image();
-	actorImg2.src = "img/iah.jpg";
-	var c2 = document.getElementById("canvas");
-	context2 = c2.getContext("2d");
-	//context2.translate(15,0);
-	context2.drawImage(actorImg2, 0, 0);
-
-	document.querySelector("#mag_alpha").innerHTML = "alpha = " + alpha;
-	document.querySelector("#mag_beta").innerHTML = "beta = " + beta;
-	document.querySelector("#mag_gamma").innerHTML = "gamma = " + gamma;
-	
-}
+window.addEventListener("deviceorientation", function(event) {
+	document.querySelector("#mag_alpha").innerHTML = "alpha = " + event.alpha;
+	document.querySelector("#mag_beta").innerHTML = "beta = " + event.beta;
+	document.querySelector("#mag_gamma").innerHTML = "gamma = " + event.gamma;
+}, true);
