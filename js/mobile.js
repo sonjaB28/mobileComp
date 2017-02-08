a174d11178fe1feff7322b9045f06e514c148b04
sonjaB28 <burkart.sonja@gmail.com>
2017-02-08 18:06:30 +0100

    reconstruct js 10

--- js/mobile.js
+++ js/mobile.js
@@ -59,14 +59,112 @@ window.addEventListener("deviceorientation", function(event) {
 		doCalibrate = false;
 	}
 
-	drawActor();
+	update(alpha-alpha_standard, beta-beta_standard, gamma-gamma_standard);
+	//drawActor();
 
 	document.querySelector("#mag_alpha").innerHTML = "alpha = " + alpha;
 	document.querySelector("#mag_beta").innerHTML = "beta = " + beta;
 	document.querySelector("#mag_gamma").innerHTML = "gamma = " + gamma;
 }, true);
 
-
 function drawActor() {
 	context.drawImage(actorImg, 0, 0);
+}
+
+function update(alpha, beta, gamma) {	
+	updatePosition(x_pos, y_pos, gamma, beta);
+	
+	context.clearRect(0,0,actorWidth, actorHeight);
+	context.setTransform(1,0,0,1, x_pos, y_pos);
+		
+	drawActor();
+}
+
+function updatePosition(x, y, gamma, beta) {
+	var x_dir = getDirection(gamma);
+	var y_dir = getDirection(beta);	
+	
+	// check for wall in x direction
+	var slammedWall = false;
+	if(x_dir != 0) {		
+		for(var i=1; i <= movement; i++) {
+			if(!slammedWall) {
+				// move to left
+				if(x_dir < 0) {
+					if(slamsAWall(x-i,y, true)) {
+						slammedWall = true;
+						x_pos = x-i+1;
+					} else {
+						x_pos = x-i;
+					}				
+				}
+				// move to right
+				else {
+					if(slamsAWall(x+actorWidth+i, y, true)) {
+						slammedWall = true;
+						x_pos = x+i-1;
+					} else {
+						x_pos = x+i;
+					}
+				}
+			}
+		
+		}
+	}
+	//check for wall in y direction
+	slammedWall = false;
+	if(y_dir != 0) {		
+		for(var i=1; i <= movement; i++) {
+			if(!slammedWall) {
+				// move up
+				if(y_dir < 0) {
+					if(slamsAWall(x,y-i, false)) {
+						slammedWall = true;
+						y_pos = y-i+1;
+					} else {
+						y_pos = y-i;
+					}				
+				}
+				// move down
+				else {
+					if(slamsAWall(x, y+actorHeight+i, false)) {
+						slammedWall = true;
+						y_pos = y+i-1;
+					} else {
+						y_pos = y+i;
+					}
+				}
+			}
+		
+		}
+	}
+}
+
+function getDirection(orientation) {
+	if(orientation < -range) {
+		return -1;
+	} else if(orientation > range) {
+		return  1;
+	} else {
+		return 0;	
+	}
+}
+
+function slamsAWall(x, y, check_vertical) {
+	if(check_vertical) {
+		for(var p = 0; p < actorHeight; p++) {
+			var index = ((y+p)*canvasWidth+x)*4;
+			if(imgData.data[index] < 50 && imgData.data[index+1] < 50 && imgData.data[index+2] < 50) {
+				return true;
+			}
+		}
+	} else {
+		for(var q = 0; q < actorWidth; q++) {
+			var index = (y*canvasWidth+x+q)*4;
+			if(imgData.data[index] < 50 && imgData.data[index+1] < 50 && imgData.data[index+2] < 50) {
+				return true;
+			}
+		}
+	}
+	return false;	
 }
