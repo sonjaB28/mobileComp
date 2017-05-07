a330ed5b4c3c41571648aff6d53bdb696ffb58fd
sonjaB28 <burkart.sonja@gmail.com>
2017-05-07 18:19:02 +0200

    hide navbar

--- js/script.js
+++ js/script.js
@@ -307,10 +307,3 @@ function min(a, b) {
 function addText() {
 	document.getElementById("text").innerHTML += ", " + "LALALALALA" + " & " + "";
 }
-window.addEventListener("load",function() {
-	// Set a timeout...
-	setTimeout(function(){
-		// Hide the address bar!
-		window.scrollTo(0, 1);
-	}, 0);
-});
