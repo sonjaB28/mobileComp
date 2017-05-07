cb305f87e561e8d092285e66c2c3413510589019
sonjaB28 <burkart.sonja@gmail.com>
2017-05-07 17:02:13 +0200

    added movement

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // global variables 
 doCalibrate = true;
 paused = false;
+
 // constants
-min_height = 100;
-min_width = 100;
+movement = 4;
+range = 2;
 
 // preload image data
 if (document.images) {
@@ -22,12 +23,21 @@ window.onload = function() {
 
 window.onresize = function() {resize();}
 
-function init() {
+function init() {	
+	pos_x = 5;
+	pos_y = 5;
+	alpha_standard = 0;
+	beta_standard = 0;
+	gamma_standard = 0;	
+	offset_x = 0;
+	offset_y = 0;
 	doCalibrate = true;	
 	paused = true;
 	pause();
 
 // load images	
+	actorImg = new Image();
+	actorImg.src = "img/fluffball_small.png";
 	bgImg = img1;
 	
 // load canvas
@@ -40,11 +50,16 @@ function init() {
 	context.setTransform(1,0,0,1, 0, 0);
 	context.drawImage(bgImg, 0, 0);
 	
+	canvasWidth = canvas.width;
+	canvasHeight = canvas.height; 
+	
+	actorWidth = actorImg.naturalWidth;
+	actorHeight = actorImg.naturalHeight;
 // get bg data
 	imgData = context.getImageData(0, 0, bgImg.naturalWidth, bgImg.naturalHeight);
 	
 // place context at start coordinates
-	context.setTransform(1,0,0,1, 0, 0);
+	context.setTransform(1,0,0,1, pos_x, pos_y);
 	
 // draw
 	resize();
@@ -71,6 +86,156 @@ function pause() {
 	}
 }
 
+// Keyboard Usage
+window.addEventListener("keydown", function(e) {
+	var e = window.event||e;
+	var x_axis = 0;
+	var y_axis = 0;
+	
+	
+	switch(e.keyCode) { 
+	  case 37: // left
+		x_axis = -(range+1);
+		e.preventDefault();
+	    break;
+	  case 38: // up
+		y_axis = -(range+1);
+		e.preventDefault();
+	    break;
+	  case 39: // right
+		x_axis = (range+1);
+		e.preventDefault();
+	    break;
+	  case 40: // down
+		y_axis = (range+1);
+		e.preventDefault();
+	    break;
+	}
+	update(0, y_axis, x_axis);	
+}, true);
+
+// mobile device
+window.addEventListener("deviceorientation", function(event) {
+	var alpha = event.alpha;
+	var beta = event.beta;
+	var gamma = event.gamma;
+	
+	if(doCalibrate == true) {
+		alpha_standard = alpha;
+		beta_standard = beta;
+		gamma_standard = gamma;
+		doCalibrate = false;
+	}
+
+	update(alpha-alpha_standard, beta-beta_standard, gamma-gamma_standard);
+}, true);
+
+// movement functions
+function update(alpha, beta, gamma) {	
+	if(paused) {
+		return;
+	}
+
+	updatePosition(pos_x, pos_y, gamma, beta);
+
+	draw();
+}
+
+function updatePosition(x, y, gamma, beta) {
+	var x_dir = getDirection(gamma);
+	var y_dir = getDirection(beta);	
+	
+	// check for wall in x direction
+	var slammedWall = false;
+	if(x_dir != 0) {		
+		for(var i=1; i <= movement; i++) {
+			if(!slammedWall) {
+				// move to left
+				if(x_dir < 0) {
+					if(slamsAWall(x-i,y, true)) {
+						slammedWall = true;
+						pos_x = x-i+1;
+					} else {
+						pos_x = x-i;
+					}				
+				}
+				// move to right
+				else {
+					if(slamsAWall(x+actorWidth+i, y, true)) {
+						slammedWall = true;
+						pos_x = x+i-1;
+					} else {
+						pos_x = x+i;
+					}
+				}
+			}		
+		}
+	}
+	//check for wall in y direction
+	slammedWall = false;
+	if(y_dir != 0) {		
+		for(var i=1; i <= movement; i++) {
+			if(!slammedWall) {
+				// move up
+				if(y_dir < 0) {
+					if(slamsAWall(x,y-i, false)) {
+						slammedWall = true;
+						pos_y = y-i+1;
+					} else {
+						pos_y = y-i;
+					}				
+				}
+				// move down
+				else {
+					if(slamsAWall(x, y+actorHeight+i, false)) {
+						slammedWall = true;
+						pos_y = y+i-1;
+					} else {
+						pos_y = y+i;
+					}
+				}
+			}		
+		}
+	}
+}
+function slamsAWall(x, y, check_vertical) {
+	if(check_vertical) {
+		for(var p = 0; p < actorHeight; p++) {
+			var index = ((y+p)*canvasWidth+x)*4;
+			if(imgData.data[index] < 50 && imgData.data[index+1] < 50 && imgData.data[index+2] < 50) {
+				return true;
+			}
+		}
+	} else {
+		for(var q = 0; q < actorWidth; q++) {
+			var index = (y*canvasWidth+x+q)*4;
+			if(imgData.data[index] < 50 && imgData.data[index+1] < 50 && imgData.data[index+2] < 50) {
+				return true;
+			}
+		}
+	}
+	return false;	
+}
+function getDirection(orientation) {
+	if(orientation < -range) {
+		return -1;
+	} else if(orientation > range) {
+		return  1;
+	} else {
+		return 0;	
+	}
+}
+
+// movement of the background
+function updateBackgroundPosition() {	
+	if(pos_x > context.canvas.width/2 && pos_x < canvasWidth-context.canvas.width/2) {
+		offset_x = context.canvas.width/2-pos_x;
+	}
+	if(pos_y > context.canvas.height/2 && pos_y < canvasHeight-context.canvas.height/2) {
+		offset_y = context.canvas.height/2-pos_y;
+	}
+}
+
 // drawing functions
 function resize() {
 	
@@ -87,10 +252,13 @@ function resize() {
 }
 
 function draw() {
+	//updateBackgroundPosition();
 	context.setTransform(1,0,0,1, 0, 0);
 	context.clearRect(0,0, context.canvas.width, context.canvas.height);
 	context.setTransform(1,0,0,1, 0, 0);
-	context.drawImage(bgImg, 0, 0);
+	context.drawImage(bgImg, offset_x, offset_y);
+	context.setTransform(1,0,0,1, pos_x, pos_y);
+	context.drawImage(actorImg, offset_x, offset_y);
 }
 
 
@@ -104,8 +272,5 @@ function min(a, b) {
 	}
 }
 function addText() {
-	var help = document.body.clientHeight
-			-document.getElementById("header_game").clientHeight
-			-1.2*document.getElementById("footer_game").clientHeight;
-	document.getElementById("text").innerHTML += ", " + help + " & " + "";
+	document.getElementById("text").innerHTML += ", " + "LALALALALA" + " & " + "";
 }
