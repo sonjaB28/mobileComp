5e63e2aa474125ca01b30c1fd10a63c07df7be00
sonjaB28 <burkart.sonja@gmail.com>
2017-05-08 22:11:50 +0200

    timeou

--- js/script.js
+++ js/script.js
@@ -427,14 +427,6 @@ navigator.wakeLock.request("display").then(
     function errorFunction() {
         // error
     }
-//here system indicates CPU, GPU, radio, wifi etc.
-navigator.wakeLock.request("system").then(
-    function successFunction() {
-        // success
-    },
-    function errorFunction() {
-        // error
-    }
 );
 
 //release() is used to release the lock.
