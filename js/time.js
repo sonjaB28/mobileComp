449863b6e9ed30a8eaf98d6c1d5b4736a71bdd47
sonjaB28 <burkart.sonja@gmail.com>
2017-05-23 09:06:43 +0200

    remodeled preview imgs

--- js/time.js
+++
@@ -1,7 +0,0 @@
-var counter = 3;
-for(var i=3; i>=0; i--) {
-	document.getElementById("countdown").innerHTML = counter;
-	setTimeout(function(){}, 1000);
-}
-
-setTimeout(function(){ alert("Hello"); }, 1000);
